0880929f92b04a1f362c69afcafae4f67d01abba
aluno <aluno@debian.EDUC123.sp.senai.br>
2025-11-05 16:33:39 -0300

    Added new text into README.md and a new Python file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mediaaritmeticadebian
+Neste repositório você encontrará um código Python simples sobre média!
+![](https://i.pinimg.com/736x/8e/63/f2/8e63f2bf724c7f000401681bd655a5d4.jpg)
---
+++ codigo.py
@@ -0,0 +1,7 @@
+num1 = float(input('Informe sua primeira nota: '))
+num2 = float(input('Informe sua primeira nota: '))
+
+media = (num1 + num2)/2
+
+print(f'Sua média de notas foi de {media}')
+
